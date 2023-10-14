248ddf75a23eb601aa2310ff8514285f939cdf6b
M. Samiul Hasnat <58338543+MSamiulHasnat@users.noreply.github.com>
2023-10-14 10:08:52 +0600

    Update Update_and_Print.c

--- Intro2ProgrammingLanguage/Week2/Mod8 Assignment2/Update_and_Print.c
+++ Intro2ProgrammingLanguage/Week2/Mod8 Assignment2/Update_and_Print.c
@@ -1,54 +1,21 @@
 #include <stdio.h>
 
-//Compiler version gcc  6.3.0
+int main(){
+    //write your code here
+    int n;
+    scanf("%d", &n);
 
-int main()
-{
-  int i;
-  int n;
-  int ar[n];
-  int x,v;
-  
-  scanf("%d",&n);
-  
-  for(i=1;i<=n;i++)
-  {
-    scanf("%d ",&ar[i]);
-  }
-  
-  scanf("%d %d",&x,&v);
-  
-  ar[x]=v;
-  
-  for(i=n-1;i>=0;i=i-1)
-    {
-      printf("%d ",ar[i]);
+    int array[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &array[i]);
     }
     
-  return 0;
+    int x, v;
+    scanf("%d %d ", &x, &v);
+    
+    array[x] = v;
+
+    for(int i = n-1; i >= 0; i--){
+        printf("%d ", array[i]);
+    }
 }
-
-
-
-
-// #include <stdio.h>
-
-// int main(){
-//     //write your code here
-//     int n;
-//     scanf("%d", &n);
-
-//     int array[n];
-//     for(int i = 0; i < n; i++){
-//         scanf("%d", &array[i]);
-//     }
-    
-//     int x, v;
-//     scanf("%d %d ", &x, &v);
-    
-//     array[x] = v;
-
-//     for(int i = n-1; i >= 0; i--){
-//         printf("%d ", array[i]);
-//     }
-// }
